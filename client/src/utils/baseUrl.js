c3080fcf15d40f064d77facaa93a0cc2dc46cd4c
curtisgry <cgray.dev@gmail.com>
2021-09-22 19:50:15 -0400

    deploy with bootstrap changes

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-// export const baseUrl = "https://calm-wave-18798.herokuapp.com";
+export const baseUrl = "https://calm-wave-18798.herokuapp.com";
 
 
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
